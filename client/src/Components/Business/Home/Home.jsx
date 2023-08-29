f8bdbf1727d3ef02c180e982da663ca4aaed84fb
Hetvik Shah <hetvikshah11@gmail.com>
2023-08-29 19:37:36 +0530

    7:37 pm

--- client/src/Components/Business/Home/Home.jsx
+++ client/src/Components/Business/Home/Home.jsx
@@ -1,29 +1,122 @@
-import React, {useEffect} from 'react' ;
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.css'
+import './List.css'
 
 import { useAuth } from '../../../contexts/AuthContext'
 
 const Home = () => {
 
-  const {userType, logout} = useAuth() ;
-  const navigate = useNavigate() ; 
+  const { userType, logout } = useAuth();
+  const navigate = useNavigate();
 
-  useEffect(()=> {
-    const authenticate = async ()=>{
-      if(userType == null){
+  useEffect(() => {
+    const authenticate = async () => {
+      if (userType == null) {
         try {
-          await logout() ;
-          navigate('/') ;
+          await logout();
+          navigate('/');
         } catch (error) {
           console.log(error);
         }
       }
     }
-    authenticate() ;
-  },[])
+    authenticate();
+  }, [])
 
   return (
-    <h2>Business Home</h2>
+    <>
+      <div className="main">
+        <div className="background">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">
+                    Home <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">
+                    Disabled
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="content1">
+            <div className="content1_left">
+              <div className="thumbnail_pic">
+                <img src="Thumbnail.jpg" className="thumbnail_img" />
+                <div className="thumbnail_img">Black</div>
+              </div>
+            </div>
+            <div className="content1_right">
+              <h2>Name of restautant</h2>
+              <h6>Sitting Capacity</h6>
+              <h6>Google Map URL</h6>
+              <h6>Extra details</h6>
+              <h6>Extra details</h6>
+              <h6>Extra details</h6>
+              {/* Menu, Clock, Orders, Photos,  */}
+            </div>
+          </div>
+          <div className="content2">
+            <h5>Waiting Time(in minutes)</h5>
+            <input type="number" defaultValue={0} />
+            <button>Update Waiting time</button>
+            <br />
+            <br />
+            <div>
+              <button>Update Menu</button>
+            </div>
+            <br />
+            <br />
+            <div className="resrved_tables">
+              <div id="myDIV" className="header">
+                <h2>List of Waiting Customers</h2>
+                <input type="text" id="myInput" placeholder="Title..." />
+                <span onclick="newElement()" className="addBtn">
+                  Add
+                </span>
+              </div>
+              <ul id="myUL">
+                <li>Hit the gym</li>
+                <li className="checked">Pay bills</li>
+                <li>Meet George</li>
+                <li>Buy eggs</li>
+                <li>Read a book</li>
+                <li>Organize office</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
