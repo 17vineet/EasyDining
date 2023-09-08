40df58ebf7dd5ef2bf2467baea6a1e2992a142fb
Hetvik Shah <hetvikshah11@gmail.com>
2023-09-08 17:20:02 +0530

    5:19

--- client/src/Components/Business/Home/WaitingList.jsx
+++ client/src/Components/Business/Home/WaitingList.jsx
@@ -1,78 +1,78 @@
 import { useState, useEffect } from 'react';
 import './WaitingList.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import API from '../../../axios' ;
-import Delete from '@mui/icons-material/Delete' ;
+import API from '../../../axios';
+import Delete from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import Loading from '../../Loading';
-import {useAuth} from '../../../contexts/AuthContext' ;
+import { useAuth } from '../../../contexts/AuthContext';
 
 function WaitingList() {
   const [name, setName] = useState('')
-  const [loe, setLoe] = useState([]) ;
-  const {currentUser} = useAuth() ;
-  const [isLoading,setIsLoading]=useState(false)
+  const [loe, setLoe] = useState([]);
+  const { currentUser } = useAuth();
+  const [isLoading, setIsLoading] = useState(false)
 
- async function handleClick() {
-    if(name.trim().length!=0){
-    setIsLoading(true);
-      await API.post('/restaurant/insertWaitingList', { rid: currentUser._id, name : name })
-      setLoe((prev)=>[...prev, name]);
+  async function handleClick() {
+    if (name.trim().length != 0) {
+      setIsLoading(true);
+      await API.post('/restaurant/insertWaitingList', { rid: currentUser._id, name: name })
+      setLoe((prev) => [...prev, name]);
       setName("");
-    setIsLoading(false);
+      setIsLoading(false);
 
     }
-    else{
+    else {
       alert("Enter a proper name")
     }
-   
+
   }
   function handleChange(event) {
     setName(event.target.value)
   }
   async function handleDelete(index) {
     setIsLoading(true);
-    const resp=await API.post('/restaurant/removeWaitingCustomer', {rid : currentUser._id, index}) ;
-    const updatedList = loe.filter((_,ind) => index != ind)
+    const resp = await API.post('/restaurant/removeWaitingCustomer', { rid: currentUser._id, index });
+    const updatedList = loe.filter((_, ind) => index != ind)
     setLoe(updatedList)
     setIsLoading(false);
 
   }
-  async function handleDine(index){
+  async function handleDine(index) {
     setIsLoading(true);
     handleDelete(index);
-    const resp=await API.post('/restaurant/addToDineIn',{rid:currentUser._id, cname:loe[index]});
+    const resp = await API.post('/restaurant/addToDineIn', { rid: currentUser._id, cname: loe[index] });
     console.log(resp.data);
     setIsLoading(false);
 
   }
 
-  useEffect(()=>{
-    const fetchWaitingList = async ()=>{
-      const resp = await API.post('/restaurant/getWaitingList',{rid : currentUser._id}) ; 
-      const arr=resp.data.customers.map((elem)=>elem.cname);
+  useEffect(() => {
+    const fetchWaitingList = async () => {
+      const resp = await API.post('/restaurant/getWaitingList', { rid: currentUser._id });
+      const arr = resp.data.customers.map((elem) => elem.cname);
       setLoe(arr);
     }
-    fetchWaitingList() ;
-  }, []) ;
+    fetchWaitingList();
+  }, []);
 
   return (
     < >
-    {isLoading && <Loading/>}
-     <div   >
-     <input type='text' placeholder='Enter your name to reserve table' onChange={handleChange} value={name}></input>
-      <button onClick={handleClick} className='btn btn-primary add_btn'><AddIcon/></button>
-     </div><br></br>
+      {isLoading && <Loading />}
+      <div   >
+        <input type='text' placeholder='Enter your name to reserve table' onChange={handleChange} value={name}></input>
+        <button onClick={handleClick} className='btn btn-primary add_btn'><AddIcon /></button>
+      </div><br></br>
       {
         loe.map((ele, index) => {
           return (
             <>
               <div className='element' key={index}>
                 <div className="Customer_name">{ele}</div>
-                <button className='btn btn-primary delete_btn'  onClick={() => {
+                <button className='btn btn-primary delete_btn' onClick={() => {
                   handleDelete(index)
-                }} > <Delete/> </button>
-                <button className='btn btn-primary dine_btn'  onClick={() => {
+                }} > <Delete /> </button>
+                <button className='btn btn-primary dine_btn' onClick={() => {
                   handleDine(index)
                 }} > Send to Dine</button>
               </div>
--- client/src/Components/Business/Menu/Menu.jsx
+++ client/src/Components/Business/Menu/Menu.jsx
@@ -1,38 +1,82 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
+import './Menu.css'
 
 function Menu() {
-    const [addNewCuisine,setaddNewCuisine]=useState(false)
-    const [addNewItem,setAddNewItem]=useState(false)
-    const handleChange=()=>{
-    }
-    const handleClick=()=>{
-        setAddNewItem(true)
-        
-    }
-    const handleAddNewCuisine=()=>{
-        setaddNewCuisine(true)
-    }
-    const handleAddItem=()=>{
-        setaddNewCuisine(true)
-    }
-  return (
-   <>
-   <h1>Add Menu</h1>
-  
-   <button onClick={handleAddNewCuisine}  className='btn btn-primary add_btn'>Add a new cuisine</button>
-    {addNewCuisine &&  <div   >
-     <input type='text' className="border "placeholder='Add Cuisine' onChange={handleChange}></input>
-      <button onClick={handleClick} className='btn btn-primary add_btn'>Add Cuisine</button><br/> {
-        addNewItem && <div>
-       <div className="container"> <input type='text' className="border "placeholder='Add Item' onChange={handleChange} ></input>
-         <button  className='btn btn-primary add_btn'>Add Item</button><br/> </div>
-         <h3>Xyz</h3>
-        </div>
-      }
-     </div>}
+  const [loe, setLoe] = useState([]);
+  const [addNewCuisine, setaddNewCuisine] = useState(false)
+  const [addNewItem, setAddNewItem] = useState(false)
+  const [cuisine, setCuisine] = useState("")
+  // const handleChange = () => {
+  // }
+  // const handleClick = () => {
+  //   setAddNewItem(true)
 
-   </>
+  // }
+  // const handleAddNewCuisine = () => {
+  //   setaddNewCuisine(true)
+  // }
+  // const handleAddItem = () => {
+  //   setaddNewCuisine(true)
+  // }
+  function handleChange(event) {
+    setCuisine(event.target.value)
+  }
+  function handleClick() {
+    console.log("Clicked");
+    // setLoe((prev) => [...prev, cuisine]);
+    setLoe((prev) => [...prev, { 'name': cuisine, 'items': [] }]);
+    console.log(loe);
+  }
+  function handleAddItem(index) {
+    console.log("Add item request");
+    console.log(loe);
+    setLoe((prev) => {
+      prev[index].items = [...prev[index].items, 'abcde']
+      // prev[index].items = newlist
+      return [...prev]
+    })
+    console.log(loe);
+  }
+  // [{name:value,items:[val1,val2]},{}]
+  return (
+    <>
+      <h1>Add Menu</h1>
+
+      <div>
+        <input type='text' placeholder='Enter your name to reserve table' onChange={handleChange} value={cuisine}></input>
+        <button onClick={handleClick} className='btn btn-primary add_btn'>Add Element</button>
+      </div>
+      <br></br>
+      {
+        loe.map((ele, index) => {
+          return (
+            <>
+              <div className='cuisine' key={index}>
+                <h1>{ele.name}</h1>
+                <input type="text" placeholder={'Enter the item of ' + ele.name + ' to add'} />
+                <button onClick={() => {
+                  handleAddItem(index)
+                }}> Add Item</button>
+                <br /><br />
+                {
+                  ele.items.map((e, index) => {
+                    return (
+                      <>
+                        <div>
+                          {
+                            console.log(e,index)
+                          }
+                          <h3>{e}</h3>
+                        </div>
+                      </>
+                    )
+                  })
+                }
+              </div>
+            </>)
+        })}
+    </>
   );
 }
 
